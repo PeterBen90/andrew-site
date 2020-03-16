63944ff99aa6b973d0c34989415c38f59f645a60
Peter Benvenuto <peterbenvenuto90@gmail.com>
2020-03-15 22:17:25 -0400

    lol

--- js/custom.js
+++ js/custom.js
@@ -75,6 +75,12 @@ $('#contact-form1').submit(function(e) {
 
   var $form = $(this);
   $.post($form.attr('action'), $form.serialize()).then(function() {
-    $.fileDownload('images/OnePercentGuys_Pre-ListingOrientation.pdf');
+    var file_path = 'images/OnePercentGuys_Pre-ListingOrientation.pdf';
+    var a = document.createElement('A');
+    a.href = file_path;
+    a.download = file_path.substr(file_path.lastIndexOf('/') + 1);
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
   });
 });
